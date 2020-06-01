a85bf9335751335c0c3c9a743c2f66cff1b7dc68
Chanyeong Cho <cyjo9603@naver.com>
2020-06-01 17:08:21 +0900

    [feat] db connect

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,15 @@
+import { createConnection } from 'typeorm';
+
 import app from './app';
+import ConnectionOptions from './ormConfig';
 
 const PORT = process.env.PORT || 4000;
 
 // eslint-disable-next-line no-console
 const handelAppStat = () => console.log(`Listening on port ${PORT}`);
-app.listen(PORT, handelAppStat);
+createConnection(ConnectionOptions)
+  .then(() => {
+    app.listen(PORT, handelAppStat);
+  })
+  // eslint-disable-next-line no-console
+  .catch((error) => console.error(error));
