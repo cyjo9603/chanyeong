cd152be44563b319d3fb61c466054b821db9225c
Chanyeong Cho <cyjo9603@naver.com>
2020-06-15 06:08:40 +0900

    [feat] GetUserInfo API

---
+++ back/src/api/User/GetUserInfo/GetUserInfo.resolvers.ts
@@ -0,0 +1,31 @@
+import { Resolvers } from '../../../types/resolvers';
+import User from '../../../models/User';
+import privateResolver from '../../../utils/privateResolver';
+
+/** SignIn
+ *  비밀번호 비교 후 맞을경우
+ *  id값을 사용해 refresh, access token 생성,
+ *  아닐경우 에러코드 응답
+ */
+const resolvers: Resolvers = {
+  Query: {
+    GetUserInfo: privateResolver(async (_, __, context) => {
+      try {
+        const { id } = context.req.user;
+        const user = User.findOne({ where: { id } });
+
+        return {
+          ok: true,
+          user,
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error,
+        };
+      }
+    }),
+  },
+};
+
+export default resolvers;
