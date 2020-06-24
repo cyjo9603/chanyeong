35beeeaba5d1421bbaccb07ab9d29b5b8134b8fe
Chanyeong Cho <cyjo9603@naver.com>
2020-06-24 15:37:43 +0900

    [style] Add darkmode button animation

--- front/src/component/DarkModeButton/styled.ts
+++ front/src/component/DarkModeButton/styled.ts
@@ -13,22 +13,42 @@ export const DarkModeWrapper = styled.div`
     align-items: center;
     border: 1px solid ${({ theme }) => theme.CARD_BORDER};
     border-radius: 40px;
-    width: 140px;
+    width: 40px;
     height: 40px;
     cursor: pointer;
     background-color: ${({ theme }) => theme.CHANGE_PRIMARY};
     color: ${({ theme }) => theme.LIGHT_GREY};
     font-weight: 500;
     font-size: 16px;
+    transition: width 0.5s;
+
+    & > span {
+      opacity: 0;
+      transition-property: opacity;
+      transition-delay: 0.5s;
+      width: 0;
+      white-space: nowrap;
+      overflow: hidden;
+    }
 
     & > img {
       -webkit-filter: grayscale(100%);
       filter: gray;
-      margin-right: 8px;
     }
-    &:hover > img {
-      -webkit-filter: grayscale(0%);
-      filter: none;
+
+    &:hover {
+      width: 140px;
+
+      & > img {
+        -webkit-filter: grayscale(0%);
+        filter: none;
+        margin-right: 8px;
+      }
+
+      & > span {
+        opacity: 1;
+        width: fit-content;
+      }
     }
   }
 `;
