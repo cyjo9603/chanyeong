0afba37b2db2070495985139e2aea8932342cc0e
Chanyeong Cho <cyjo9603@naver.com>
2020-07-17 15:52:15 +0900

    [style] Change Tag Style

--- front/src/component/TagCount/styled.ts
+++ front/src/component/TagCount/styled.ts
@@ -2,23 +2,24 @@ import styled from 'styled-components';
 
 export const TagWrapper = styled.div`
   display: flex;
-  align-items: center;
-  background-color: ${({ theme }) => theme.TAG_BACKGROUND};
+  justify-content: space-between;
   height: 23px;
   border-radius: 8px;
-  padding: 1px 8px;
-  margin-top: 4px;
   cursor: pointer;
+  text-align: center;
+  margin-top: 4px;
+  overflow: hidden;
 
+  & > span:first-child {
+    background-color: ${({ theme }) => theme.TAG_BACKGROUND};
+    padding: 0 14px;
+    height: 100%;
+  }
   & > span:last-child {
-    margin-left: 4px;
-    background-color: ${({ theme }) => theme.DARK_GREY};
-    color: ${({ theme }) => theme.DARK_BACKGROUND_GREY};
-    display: inline-block;
-    width: 20px;
-    height: 20px;
-    font-size: 12px;
-    border-radius: 10px;
-    text-align: center;
+    font-weight: 500;
+    height: 100%;
+    background-color: ${({ theme }) => theme.PRIMARY_COLOR};
+    color: ${({ theme }) => theme.LIGHT_GREY};
+    padding: 0 10px;
   }
 `;
