386227c2358a5d9e4a22cf0b338317b52d66f8de
chanyeong <cyjo9603@naver.com>
2020-10-21 14:46:43 +0900

    [feat] markdown paragraph 스타일 적용
    - p 태그 스타일 적용

--- front/src/component/MarkdownViewer/renderers/index.ts
+++ front/src/component/MarkdownViewer/renderers/index.ts
@@ -2,12 +2,14 @@ import code from './code';
 import inlineCode from './inlineCode';
 import blockquote from './blockquote';
 import heading from './heading';
+import paragraph from './paragraph';
 
 const renderers = {
   code,
   inlineCode,
   blockquote,
   heading,
+  paragraph,
 };
 
 export default renderers;
--- front/src/component/MarkdownViewer/styled.ts
+++ front/src/component/MarkdownViewer/styled.ts
@@ -2,31 +2,17 @@ import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
 
 export const MarkdownWrapper = styled(ReactMarkdown)`
-  & p,
-  :link,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  li,
-  td {
-    color: ${({ theme }) => theme.PRIMARY_FONT};
-  }
+  color: ${({ theme }) => theme.PRIMARY_FONT};
 
   & td {
     border: 1px solid ${({ theme }) => theme.BORDER_LINE_GREY};
   }
 
-  & p,
-  li {
+  & li {
     font-size: 14px;
     line-height: 2;
   }
-  & p {
-    margin: 18px 0;
-  }
+
   & h1,
   h2,
   h3,
