8dad80f58e269f56a2ab6241d02dceb69a0b9d78
chanyeong <cyjo9603@naver.com>
2020-11-07 20:44:45 +0900

    [refactor] 서브 타이틀 컴포넌트 아토믹 디자인 적용
    - 서브 타이틀 컴포넌트 아토믹 디자인 적용

---
+++ front/src/components/UI/atoms/SubTitle/SubTitle.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from 'react';
+
+import styled from '@theme/styled';
+
+import { SUBTITLE_WEIGHT_DEFAULT, SUBTITLE_WEIGHT_BOLD } from '.';
+
+type SubTitleAlign = 'left' | 'right' | 'center';
+type SubTitleWeight =
+  | typeof SUBTITLE_WEIGHT_DEFAULT
+  | typeof SUBTITLE_WEIGHT_BOLD;
+
+interface Props {
+  text: string;
+  weight?: SubTitleWeight;
+  align?: SubTitleAlign;
+}
+
+interface StyledProps {
+  align: SubTitleAlign;
+  weight: SubTitleWeight;
+}
+
+const SUB_TITLE_SIZE = 1;
+
+const StyledSubTitle = styled.h2<StyledProps>`
+  font-size: ${SUB_TITLE_SIZE}rem;
+  font-weight: ${({ weight }) => weight};
+  color: ${({ theme }) => theme.PRIMARY_FONT};
+  text-align: ${({ align }) => align};
+`;
+
+const SubTitle: FC<Props> = ({ text, weight, align }) => (
+  <StyledSubTitle align={align} weight={weight}>
+    {text}
+  </StyledSubTitle>
+);
+
+SubTitle.defaultProps = {
+  align: 'left',
+  weight: SUBTITLE_WEIGHT_DEFAULT,
+};
+
+export default SubTitle;
---
+++ front/src/components/UI/atoms/SubTitle/index.ts
@@ -0,0 +1,4 @@
+export { default } from './SubTitle';
+
+export const SUBTITLE_WEIGHT_DEFAULT = 500;
+export const SUBTITLE_WEIGHT_BOLD = 700;
