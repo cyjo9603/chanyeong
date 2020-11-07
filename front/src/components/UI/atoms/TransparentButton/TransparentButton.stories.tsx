fcb5809b3a248ebb45c2998aeef3de5b5924e837
chanyeong <cyjo9603@naver.com>
2020-11-07 19:58:15 +0900

    [refactor] 투명 버튼 아토믹 디자인 적용
    - 투명 버튼 아토믹 디자인 적용

---
+++ front/src/components/UI/atoms/TransparentButton/TransparentButton.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import TransparentButton from '.';
+
+export default {
+  title: 'Atoms/TransparentButton',
+  component: TransparentButton,
+};
+
+export const Default = () => {
+  return <TransparentButton></TransparentButton>;
+};
