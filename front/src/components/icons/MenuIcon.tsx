27a6935eaad38de14c50a77699097cfcf89e8ef3
chanyeong <cyjo9603@naver.com>
2020-11-08 16:41:12 +0900

    [refactor] 메뉴 아이콘 컴포넌트 추가
    - 메뉴 아이콘 컴포넌트 추가

---
+++ front/src/components/icons/MenuIcon.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+const MenuIcon: FC = () => (
+  <svg viewBox="0 0 76 66">
+    <g transform="translate(-12 -17)">
+      <g transform="translate(0 -952.362)">
+        <path d="M16,969.362a4,4,0,0,0,0,8H84a4,4,0,0,0,0-8Zm0,29a4,4,0,1,0,0,8H84a4,4,0,0,0,0-8Zm0,29a4,4,0,0,0,0,8H84a4,4,0,0,0,0-8Z" />
+      </g>
+    </g>
+  </svg>
+);
+
+export default MenuIcon;
