2893f769aab5f9324bb08e6c3359506f1fe43c07
chanyeong <cyjo9603@naver.com>
2020-11-08 20:57:11 +0900

    [refactor] 프로젝트 카드 컴포넌트 아토믹 디자인 적용
    - 프로젝트 카드 컴포넌트 아토믹 디자인 적용

---
+++ front/src/components/UI/organisms/ProjectCard/ProjectCardDescription.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+
+import styled from '@theme/styled';
+
+interface Props {
+  description: string;
+}
+
+export const StyledProjectCardDescription = styled.div`
+  width: 190px;
+  color: ${({ theme }) => theme.PRIMARY_FONT};
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+
+  @media (max-width: ${({ theme }) => theme.BP.MOBILE}) {
+    display: none;
+  }
+`;
+
+const ProjectCardDescription: FC<Props> = ({ description }) => (
+  <StyledProjectCardDescription>{description}</StyledProjectCardDescription>
+);
+
+export default ProjectCardDescription;
