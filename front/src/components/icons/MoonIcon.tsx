4a939dc495375233f3f2b9839ebbd851f90ffa3b
chanyeong <cyjo9603@naver.com>
2020-11-08 19:25:07 +0900

    [refactor] Sun, Moon 아이콘 추가
    - Sun, Moon 아이콘 추가

---
+++ front/src/components/icons/MoonIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MoonIcon = () => (
+  <svg viewBox="0 0 99.893 100">
+    <g transform="translate(-0.156 0.156)">
+      <path
+        d="M99.439,62.12a2,2,0,0,0-2.279,0A42.84,42.84,0,0,1,38.28,3.161,2,2,0,0,0,36,0a51.08,51.08,0,1,0,64,64A2,2,0,0,0,99.439,62.12Z"
+        fill="#ffd500"
+      />
+    </g>
+  </svg>
+);
+
+export default MoonIcon;
