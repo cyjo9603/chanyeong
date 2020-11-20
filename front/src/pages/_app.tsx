671517638635971b78cc83d6ebbf5c15e183e992
chanyeong <cyjo9603@naver.com>
2020-11-20 21:33:33 +0900

    [refactor] _app 아토믹 디자인 컴포넌트로 교체
    - 아토믹 디자인으로 변경한 AppLayout 교체
    - DarkModeButton또한 교체

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -8,11 +8,14 @@ import { getDataFromTree } from '@apollo/react-ssr';
 
 import { lightTheme, darkTheme } from '@theme/.';
 import GlobalStyle from '@theme/globalStyle';
-import AppLayout from '@component/AppLayout';
-import DarkModeButton from '@component/DarkModeButton';
+import AppLayout from '@frames/AppLayout';
+import DarkModeButton from '@molecules/DarkModeButton';
 import { GET_USER_INFO } from '@queries/user.queries';
 import { getAccessToken, getRefreshToken } from '@lib/cookie';
-import { reissuanceAccessToken, ERROR_EXPIRATION } from '@lib/reissuanceAccessToken';
+import {
+  reissuanceAccessToken,
+  ERROR_EXPIRATION,
+} from '@lib/reissuanceAccessToken';
 import { getUserInfo } from '@gql-types/api';
 import withApolloClient from '@src/apollo';
 
@@ -82,7 +85,10 @@ const App = ({ Component, pageProps, apollo }: Props) => {
         <Helmet>
           <title>chanyeong</title>
           <meta charSet="UTF-8" />
-          <meta name="naver-site-verification" content="2b9008defffc3f461603f5498a43a990cd8b8b65" />
+          <meta
+            name="naver-site-verification"
+            content="2b9008defffc3f461603f5498a43a990cd8b8b65"
+          />
           <meta
             name="viewport"
             content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover"
@@ -93,7 +99,10 @@ const App = ({ Component, pageProps, apollo }: Props) => {
             content="프론트엔드 개발자를 목표로 공부하고 있는 조찬영의 포트폴리오와 블로그 페이지입니다. 개발자가 되기 위해 노력한 제 이야기들을 기록해 놓았습니다."
           />
           <meta name="og:title" content="chanyeong" />
-          <meta name="og:image" content="http://image.toast.com/aaaabcy/main_image.png" />
+          <meta
+            name="og:image"
+            content="http://image.toast.com/aaaabcy/main_image.png"
+          />
           <meta
             name="og:description"
             content="프론트엔드 개발자를 목표로 공부하고 있는 조찬영의 포트폴리오와 블로그 페이지입니다. 개발자가 되기 위해 노력한 제 이야기들을 기록해 놓았습니다."
@@ -118,9 +127,9 @@ const App = ({ Component, pageProps, apollo }: Props) => {
           <link rel="apple-touch-icon-precomposed" href="/favicon2.png" />
         </Helmet>
         <GlobalStyle theme={!isDarkMode ? lightTheme : darkTheme} />
-        <AppLayout isDarkMode={isDarkMode}>
+        <AppLayout>
           <Component {...pageProps} />
-          <DarkModeButton onClickDarkMode={onClickDarkMode} isDarkMode={isDarkMode} />
+          <DarkModeButton onClick={onClickDarkMode} isDarkMode={isDarkMode} />
         </AppLayout>
       </ApolloProvider>
     </ThemeProvider>
