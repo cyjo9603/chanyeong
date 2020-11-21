c313b2d7dc53e3b1b9883bed61f3d9d39db8b769
chanyeong <cyjo9603@naver.com>
2020-11-21 21:03:50 +0900

    [refactor] 검색 페이지 아토믹 컴포넌트 적용
    - 검색 페이지 아토믹 컴포넌트 적용

--- front/src/routes/SeachBlogPost/SearchBlogPostPresenter.tsx
+++ front/src/routes/SeachBlogPost/SearchBlogPostPresenter.tsx
@@ -1,28 +1,61 @@
 import React from 'react';
 
-import PageContainer from '@component/pageContainer';
-import BlogPostCard from '@component/BlogPostCard';
+import RowFrame from '@frames/RowFrame';
+import BlogPostCard from '@organisms/BlogPostCard';
 import { searchPosts_SearchPosts_posts } from '@gql-types/api';
-import { SearchPageWrapper, NoPost } from './styled';
+import styled from '@theme/styled';
 
 interface Props {
   searchWord: string;
   posts: searchPosts_SearchPosts_posts[];
 }
 
+const StyledSearchBlogPost = styled.section`
+  margin: 0 auto;
+  margin-top: 20px;
+  width: 900px;
+
+  & > header > h1 {
+    font-size: 24px;
+    color: ${({ theme }) => theme.PRIMARY_FONT};
+    margin-bottom: 20px;
+  }
+
+  & > a > div {
+    margin-bottom: 40px;
+  }
+
+  & .empty-posts {
+    color: ${({ theme }) => theme.PRIMARY_FONT};
+    text-align: center;
+  }
+
+  @media (max-width: ${({ theme }) => theme.BP.PC}) {
+    width: 700px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.BP.TABLET}) {
+    width: 100%;
+  }
+`;
+
 const SearchBlogPostPresenter = ({ searchWord, posts }: Props) => (
-  <PageContainer>
-    <SearchPageWrapper>
+  <RowFrame>
+    <StyledSearchBlogPost>
       <header>
         <h1>검색 결과 : {searchWord}</h1>
       </header>
       {posts.length !== 0 ? (
-        posts.map((v) => <BlogPostCard key={`blog_search_${searchWord}_${v.id}`} data={v} />)
+        posts.map((v) => (
+          <BlogPostCard key={`blog_search_${searchWord}_${v.id}`} data={v} />
+        ))
       ) : (
-        <NoPost>&apos;{searchWord}&apos;와 일치하는 포스트가 없습니다.</NoPost>
+        <div className="empty-posts">
+          &apos;{searchWord}&apos;와 일치하는 포스트가 없습니다.
+        </div>
       )}
-    </SearchPageWrapper>
-  </PageContainer>
+    </StyledSearchBlogPost>
+  </RowFrame>
 );
 
 export default SearchBlogPostPresenter;
