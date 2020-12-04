e4f561d500d0f85a5cea2a0357c521e0df0b517f
chanyeong <cyjo9603@naver.com>
2020-12-04 23:01:54 +0900

    [feat] isAuth Directive schema에 등록
    - isAuth Directive schema에 등록
    - graphql 파일에 @auth 정의

--- back/src/schema.ts
+++ back/src/schema.ts
@@ -1,9 +1,18 @@
-import { makeExecutableSchema, mergeTypeDefs, mergeResolvers, loadFilesSync } from 'graphql-tools';
+import {
+  makeExecutableSchema,
+  mergeTypeDefs,
+  mergeResolvers,
+  loadFilesSync,
+} from 'graphql-tools';
 import path from 'path';
+
+import isAuthDirective from '@auth/isAuthDirective';
 
 const allTypes = loadFilesSync(path.join(__dirname, './api/**/*.graphql'));
 
-const allResolvers = loadFilesSync(path.join(__dirname, './api/**/*.resolvers.ts'));
+const allResolvers = loadFilesSync(
+  path.join(__dirname, './api/**/*.resolvers.ts'),
+);
 
 const mergedTypeDefs = mergeTypeDefs(allTypes);
 
@@ -12,6 +21,9 @@ const mergedResolvers = mergeResolvers(allResolvers);
 const schema = makeExecutableSchema({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  schemaDirectives: {
+    auth: isAuthDirective as any,
+  },
 });
 
 export default schema;
