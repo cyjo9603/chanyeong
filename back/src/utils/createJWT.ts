d7eb78178c008178b93ec60b9ad5a2932b1bd0b2
chanyeong <cyjo9603@naver.com>
2020-12-04 23:43:30 +0900

    [refactor] jwt 생성 함수 유효기간 설정방식 변경
    - jwt 생성 함수 유효기간 설정방식 변경
    - 기존처럼 수동으로 계산해 넣지 않고 jwt내부에서 지원하는 날짜 형식을 사용

--- back/src/utils/createJWT.ts
+++ back/src/utils/createJWT.ts
@@ -9,28 +9,18 @@ export const ACCESS_TOKEN = 'ACCESS_TOKEN' as const;
 
 export const createRefreshToken = (id: number) => {
   if (JWT_TOKEN) {
-    const token = jwt.sign(
-      {
-        type: REFRESH_TOKEN,
-        id,
-        exp: new Date().getTime() + 1000 * 60 * 60 * 12,
-      },
-      JWT_TOKEN,
-    );
+    const token = jwt.sign({ type: REFRESH_TOKEN, id }, JWT_TOKEN, {
+      expiresIn: '7d',
+    });
     return token;
   }
 };
 
 export const createAccessToken = (id: number) => {
   if (JWT_TOKEN) {
-    const token = jwt.sign(
-      {
-        type: ACCESS_TOKEN,
-        id,
-        exp: new Date().getTime() + 1000 * 60 * 30,
-      },
-      JWT_TOKEN,
-    );
+    const token = jwt.sign({ type: ACCESS_TOKEN, id }, JWT_TOKEN, {
+      expiresIn: '15m',
+    });
     return token;
   }
 };
