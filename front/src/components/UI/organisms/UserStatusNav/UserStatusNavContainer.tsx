79aab4a9231615a96ab1125d5dfb3d4a301fc002
chanyeong <cyjo9603@naver.com>
2020-12-05 15:20:02 +0900

    [refactor] cookie 모듈 제거
    - 이제 클라이언트에서 cookie 를 제어하지 않음

--- front/src/components/UI/organisms/UserStatusNav/UserStatusNavContainer.tsx
+++ front/src/components/UI/organisms/UserStatusNav/UserStatusNavContainer.tsx
@@ -1,8 +1,8 @@
 import React, { FC, useCallback } from 'react';
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 
+import { useReissueMutation } from '@hooks/useApollo';
 import { LOG_OUT, GET_LOCAL_USER } from '@queries/client';
-import { clearCookie, getAccessToken } from '@lib/cookie';
 import UserStatusNavPresenter from './UserStatusNavPresenter';
 
 interface Props {
@@ -11,18 +11,11 @@ interface Props {
 
 const Header: FC<Props> = ({ statusHidden }) => {
   const { data } = useQuery(GET_LOCAL_USER);
-  const [logoutMutation] = useMutation(LOG_OUT);
+  const [logoutMutation] = useReissueMutation(LOG_OUT);
 
   const onClickLogout = useCallback(() => {
-    logoutMutation({
-      context: {
-        headers: {
-          'X-JWT': getAccessToken(),
-        },
-      },
-    });
-    clearCookie();
-  }, [getAccessToken()]);
+    logoutMutation();
+  }, []);
 
   return (
     <UserStatusNavPresenter
