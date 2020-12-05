a83fbe5e3303b873fbfe9cd66019f8339a858749
chanyeong <cyjo9603@naver.com>
2020-12-05 15:22:23 +0900

    [refactor] 클라이언트 복호화 모듈 제거
    - 클라이언트에서 복호화 모듈을 사용하는 로직이 없어 복호화 모듈 제거

--- front/src/lib/crypto.ts
+++ front/src/lib/crypto.ts
@@ -2,6 +2,5 @@ import crypto from 'crypto-js';
 
 const { LOGIN_SECRET_KEY } = process.env;
 
-export const encryptValue = (value: string) => crypto.AES.encrypt(value, LOGIN_SECRET_KEY).toString();
-
-export const decryptValue = (value: string) => crypto.AES.decrypt(value, LOGIN_SECRET_KEY).toString(crypto.enc.Utf8);
+export const encryptValue = (value: string) =>
+  crypto.AES.encrypt(value, LOGIN_SECRET_KEY).toString();
