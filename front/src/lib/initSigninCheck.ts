e362bc37557b3b93e03b2f46f0898aa7d2d76e1e
chanyeong <cyjo9603@naver.com>
2021-01-09 17:38:40 +0900

    [refactor] 초기 로그인 확인 로직 수정
    - 기존 토큰 만료 후 재발급 요청을 제거 (이전 커밋에서 apollo link 에 해당 로직 등록)
    - apollo client를 매개변수로 넣어주지 않고 직접 접근

--- front/src/lib/initSigninCheck.ts
+++ front/src/lib/initSigninCheck.ts
@@ -1,41 +1,17 @@
-/* eslint-disable @typescript-eslint/no-use-before-define */
-import { ApolloClient } from '@apollo/client';
-
+import { initializeApollo } from '@src/apollo';
 import { signInUser } from '@store/userInfo';
-import { REISSUANCE_ACCESS_TOKEN, GET_USER_INFO } from '@queries/user.queries';
-import { reissuanceAccessToken, getUserInfo } from '@gql-types/api';
+import { GET_USER_INFO } from '@queries/user.queries';
+import { getUserInfo } from '@gql-types/api';
 
-const initSigninCheck = async (apollo: ApolloClient<object>) => {
-  try {
-    await signinCheck();
-  } catch (error) {
-    await errorHanler(error).catch((err) => console.error(err));
-  }
-
-  async function signinCheck() {
-    const { data } = await apollo.query<getUserInfo>({
-      query: GET_USER_INFO,
-      fetchPolicy: 'no-cache',
-    });
-    if (data.GetUserInfo.ok) {
-      const { familyName, givenName } = data.GetUserInfo.user;
-      signInUser(`${familyName}${givenName}`);
-    }
-  }
-
-  async function errorHanler(error) {
-    const { statusCode } = error.networkError || {};
-    if (statusCode !== 401) {
-      return;
-    }
-
-    const { data } = await apollo.mutate<reissuanceAccessToken>({
-      mutation: REISSUANCE_ACCESS_TOKEN,
-    });
-
-    if (data.ReissuanceAccessToken.ok) {
-      await signinCheck();
-    }
+const initSigninCheck = async () => {
+  const apollo = initializeApollo();
+  const { data } = await apollo.query<getUserInfo>({
+    query: GET_USER_INFO,
+    fetchPolicy: 'no-cache',
+  });
+  if (data.GetUserInfo.ok) {
+    const { familyName, givenName } = data.GetUserInfo.user;
+    signInUser(`${familyName}${givenName}`);
   }
 };
 
--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -23,7 +23,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
     const mode = localStorage.getItem('mode');
     setIsDarkMode(mode === 'true');
-    initSigininCheck(apollo);
+    initSigininCheck();
   }, []);
 
   return (
