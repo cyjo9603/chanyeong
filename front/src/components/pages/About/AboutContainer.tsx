6037430dd77aa9d9673582a7c05b2cf29a479c5b
chanyeong <cyjo9603@naver.com>
2021-01-10 17:19:07 +0900

    [refactor] About 페이지 next 버전에 따라 구조 변경

--- front/src/components/pages/About/AboutContainer.tsx
+++ front/src/components/pages/About/AboutContainer.tsx
@@ -2,23 +2,22 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { useReactiveVar } from '@apollo/client';
 
 import { userInfoVar } from '@store/userInfo';
-import { initializeApollo } from '@src/apollo';
 import {
   getSkills_GetSkills_skill,
-  getAbouts_GetExperiences,
-  getAbouts_GetGroupedSkills,
+  getAbouts_GetExperiences_experiences as Experiences,
+  getAbouts_GetGroupedSkills_skills as Skills,
 } from '@gql-types/api';
-import { GET_ABOUTS } from '@queries/about.queries';
+
 import UpdateSkillForm from '@modals/UpdateSkillForm';
 import AddSkillForm from '@modals/AddSkillForm';
 import AboutPresenter from './AboutPresenter';
 
 interface Props {
-  GetExperiences: getAbouts_GetExperiences;
-  GetGroupedSkills: getAbouts_GetGroupedSkills;
+  experiences: Experiences[];
+  skills: Skills;
 }
 
-const AboutContainer = ({ GetExperiences, GetGroupedSkills }: Props) => {
+const AboutContainer = ({ experiences, skills }: Props) => {
   const userInfo = useReactiveVar(userInfoVar);
   const [openAddSkill, setOpenAddSkill] = useState(false);
   const [editSkillData, setEditSkillData] = useState<getSkills_GetSkills_skill | null>(null);
@@ -48,10 +47,10 @@ const AboutContainer = ({ GetExperiences, GetGroupedSkills }: Props) => {
     <>
       <AboutPresenter
         userName={userInfo.userName}
-        frontSkills={GetGroupedSkills?.skills.front || []}
-        backSkills={GetGroupedSkills?.skills.back || []}
-        devopsSkills={GetGroupedSkills?.skills.devops || []}
-        experiences={GetExperiences?.experiences || []}
+        frontSkills={skills?.front || []}
+        backSkills={skills?.back || []}
+        devopsSkills={skills?.devops || []}
+        experiences={experiences || []}
         editSkillData={editSkillData}
         onClickAddSkill={onClickAddSkill}
         onClickEditSkill={onClickEditSkill}
@@ -62,15 +61,6 @@ const AboutContainer = ({ GetExperiences, GetGroupedSkills }: Props) => {
       )}
     </>
   );
-};
-
-AboutContainer.getInitialProps = async () => {
-  const apolloClient = initializeApollo();
-  const { data } = await apolloClient.query({
-    query: GET_ABOUTS,
-    fetchPolicy: 'no-cache',
-  });
-  return data;
 };
 
 export default AboutContainer;
--- front/src/pages/about/index.ts
+++ front/src/pages/about/index.ts
@@ -1,1 +1,21 @@
+import { GetServerSideProps } from 'next';
+
+import { initializeApollo } from '@src/apollo';
+import { GET_ABOUTS } from '@queries/about.queries';
+import { getAbouts } from '@gql-types/api';
+
 export { default } from '@pages/About';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const apolloClient = initializeApollo();
+  const { data } = await apolloClient.query<getAbouts>({
+    query: GET_ABOUTS,
+    fetchPolicy: 'no-cache',
+  });
+  return {
+    props: {
+      experiences: data.GetExperiences.experiences,
+      skills: data.GetGroupedSkills.skills,
+    },
+  };
+};
