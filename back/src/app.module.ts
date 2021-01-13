49f36f22c7cced6880bfced0a135d068e5998263
chanyeong <cyjo9603@naver.com>
2021-01-13 18:24:41 +0900

    [chore] nestjs에 sequelize 연결
    - sequelize 관련 설정 .env에 저장
    - 기본 timestamp false 로 설정

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 const prod = process.env.NODE_ENV === 'production';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [],
+      define: {
+        timestamps: false,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      },
+    }),
     GraphQLModule.forRoot({
       playground: !prod,
       autoSchemaFile: true,
