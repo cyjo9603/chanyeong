afde270ddb7d0fccfbe91d7fd35be47fffe9ed62
chanyeong <cyjo9603@naver.com>
2021-01-14 02:56:03 +0900

    [feat] getTags Query 구현
    - 태그 횟수별로 정렬해 조회하는 getTags Query 구현

---
+++ back/src/tags/dto/getTags.dto.ts
@@ -0,0 +1,16 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+import { CoreResponse } from '@common/dtos/coreResponse.dto';
+import { Tag } from '../tags.model';
+
+@ObjectType()
+export class TagWithCount extends Tag {
+  @Field((type) => Int)
+  count!: number;
+}
+
+@ObjectType()
+export class GetTagsResponse extends CoreResponse {
+  @Field((type) => [TagWithCount], { nullable: true })
+  tags?: TagWithCount[];
+}
