4c32387bbc5fe3067b49be400782fc2bcfed2ee6
chanyeong <cyjo9603@naver.com>
2021-01-13 17:29:36 +0900

    [chore] apollo server 및 express 미들웨어 기본 설정
    - apollo graphql server 기본 설정
    - express 보안 모듈 및 기본 미들웨어 적용

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+const prod = process.env.NODE_ENV === 'production';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: !prod,
+      autoSchemaFile: true,
+      cors: {
+        credentials: true,
+        origin: prod ? /chanyeong\.com$/ : 'http://localhost:3060',
+      },
+      context: (ctx) => ({ ...ctx }),
+    }),
+  ],
 })
 export class AppModule {}
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,10 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import logger from 'morgan';
+import compression from 'compression';
+import hpp from 'hpp';
+import cookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
+
 import { AppModule } from '@/app.module';
 
+const prod = process.env.NODE_ENV === 'production';
 const PORT = process.env.PORT || 4000;
+
+const WINDOW_MS = 60 * 1000;
+const MAX_REQUEST_PER_15 = 80;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (prod) {
+    app.use(hpp());
+    app.use(helmet());
+    app.use(logger('combined'));
+    app.use(rateLimit({ windowMs: WINDOW_MS, max: MAX_REQUEST_PER_15 }));
+  } else {
+    app.use(logger('dev'));
+  }
+
+  app.use(cookieParser());
+  app.use(compression());
+
   await app.listen(PORT);
 }
+
 bootstrap();
