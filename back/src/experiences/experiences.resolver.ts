d0830f4b4e5cb5b394caae5382a2f522f4775d36
chanyeong <cyjo9603@naver.com>
2021-01-14 23:31:36 +0900

    [feat] addExperience Mutation 구현
    - 경험을 추가하는 addExperience Mutation 구현

--- back/src/experiences/experiences.resolver.ts
+++ back/src/experiences/experiences.resolver.ts
@@ -1,7 +1,11 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 
+import { CoreResponse } from '@common/dtos/coreResponse.dto';
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
 import { ExperiencesService } from './experiences.service';
 import { GetExperiences } from './dto/getExperiences.dto';
+import { AddExperienceRequest } from './dto/addExperience.dto';
 
 @Resolver()
 export class ExperiencesResolver {
@@ -14,5 +18,12 @@ export class ExperiencesResolver {
     if (!experiences || error) return { ok: false, error };
 
     return { ok: true, experiences };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation((returns) => CoreResponse)
+  async addExperience(@Args('input') input: AddExperienceRequest): Promise<CoreResponse> {
+    const { ok, error } = await this.experiencesService.add(input);
+    return { ok, error };
   }
 }
