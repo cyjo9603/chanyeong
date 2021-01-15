bdedb39b4bcb2a76a2e73d797f212b4285943f1e
chanyeong <cyjo9603@naver.com>
2021-01-15 23:41:37 +0900

    [refactor] dto name 통일 및 타입 재정의
    - dto name 통일화
    - dto 타입 intersectionType을 통해 재사용성 향상

--- back/src/projects/dto/getProjects.dto.ts
+++ back/src/projects/dto/getProjects.dto.ts
@@ -2,7 +2,7 @@ import { InputType, Field } from '@nestjs/graphql';
 
 import { ProjectType } from '../projects.model';
 
-@InputType('InputProjectsType')
+@InputType('InputGetProjects')
 export class GetProjectsRequest {
   @Field(() => ProjectType)
   type!: ProjectType;
--- back/src/skills/dto/updateSkill.dto.ts
+++ back/src/skills/dto/updateSkill.dto.ts
@@ -1,26 +1,9 @@
-import { InputType, Field, Int } from '@nestjs/graphql';
-import { SkillType } from '../skills.model';
+import { InputType, PartialType, IntersectionType, PickType } from '@nestjs/graphql';
+
+import { Skill } from '../skills.model';
 
 @InputType('InputUpdateSkill')
-export class UpdateSkillRequest {
-  @Field((type) => Int)
-  id!: number;
-
-  @Field((type) => String, { nullable: true })
-  name?: string;
-
-  @Field((type) => SkillType, { nullable: true })
-  type?: SkillType;
-
-  @Field((type) => Int, { nullable: true })
-  level?: number;
-
-  @Field((type) => String, { nullable: true })
-  description?: string;
-
-  @Field((type) => String, { nullable: true })
-  icon?: string;
-
-  @Field((type) => Int, { nullable: true })
-  order?: number;
-}
+export class UpdateSkillRequest extends IntersectionType(
+  PickType(PartialType(Skill), ['name', 'type', 'level', 'description', 'icon', 'order']),
+  PickType(Skill, ['id']),
+) {}
