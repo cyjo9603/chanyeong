3ca2488354d5b9a586a6f7d7eb0dbd8243de0933
chanyeong <cyjo9603@naver.com>
2021-01-15 17:50:09 +0900

    [feat] addProject Mutation 구현
    - 프로젝트를 추가하는 addProject Mutation 구현

--- back/src/projects/projects.resolver.ts
+++ back/src/projects/projects.resolver.ts
@@ -1,16 +1,20 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { CoreResponse } from '@common/dtos/coreResponse.dto';
 import { ProjectsService } from './projects.service';
 import { GetProjectsRequest } from './dto/getProjects.dto';
 import { ProjectsResponse } from './dto/projectResponse.dto';
 import { GetProjectRequest, GetProjectResponse } from './dto/getProject.dto';
+import { AddProjectRequest } from './dto/addProject.dto';
 
 @Resolver()
 export class ProjectsResolver {
   constructor(private projectsService: ProjectsService) {}
 
   @Query((returns) => GetProjectResponse)
-  async getProject(@Args('input') input: GetProjectRequest) {
+  async getProject(@Args('input') input: GetProjectRequest): Promise<GetProjectResponse> {
     const { project, error } = await this.projectsService.getById(input.id);
 
     if (!project || error) return { ok: false, error };
@@ -19,7 +23,7 @@ export class ProjectsResolver {
   }
 
   @Query((returns) => ProjectsResponse)
-  async getProjects(@Args('input') input: GetProjectsRequest) {
+  async getProjects(@Args('input') input: GetProjectsRequest): Promise<ProjectsResponse> {
     const { projects, error } = await this.projectsService.getAllByType(input.type);
 
     if (!projects || error) return { ok: false, error };
@@ -34,5 +38,12 @@ export class ProjectsResolver {
     if (!projects || error) return { ok: false, error };
 
     return { ok: true, projects };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation((returns) => CoreResponse)
+  async addProject(@Args('input') input: AddProjectRequest): Promise<CoreResponse> {
+    const { ok, error } = await this.projectsService.add(input);
+    return { ok, error };
   }
 }
