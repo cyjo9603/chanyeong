e93afedf820e50f22d58219d6ebad20624f358ed
chanyeong <cyjo9603@naver.com>
2021-01-16 20:51:07 +0900

    [delete] toast editor code highlighter 제거
    - 뷰어에서 사용하는 code ghighlighter 를 따로 구현하였으므로 기존 뷰어 스타일 제거

--- front/src/components/UI/organisms/TUIEditor/TUIEditor.tsx
+++ front/src/components/UI/organisms/TUIEditor/TUIEditor.tsx
@@ -1,22 +1,6 @@
 import React from 'react';
 import { Editor, EditorProps } from '@toast-ui/react-editor';
-import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
-import hljs from 'highlight.js/lib/highlight';
-
-import javascript from 'highlight.js/lib/languages/javascript';
-import typescript from 'highlight.js/lib/languages/typescript';
-import bash from 'highlight.js/lib/languages/bash';
-import css from 'highlight.js/lib/languages/css';
-import html from 'highlight.js/lib/languages/htmlbars';
-import json from 'highlight.js/lib/languages/json';
-
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('typescript', typescript);
-hljs.registerLanguage('bash', bash);
-hljs.registerLanguage('css', css);
-hljs.registerLanguage('html', html);
-hljs.registerLanguage('json', json);
 
 export interface TUIEditorWithForwardedProps extends EditorProps {
   forwardedRef?: React.MutableRefObject<Editor>;
@@ -24,11 +8,7 @@ export interface TUIEditorWithForwardedProps extends EditorProps {
 
 const TUIEditor = (props: TUIEditorWithForwardedProps) => (
   <>
-    <Editor
-      plugins={[codeSyntaxHighlight.bind(hljs), colorSyntax]}
-      {...props}
-      ref={props.forwardedRef}
-    />
+    <Editor plugins={[colorSyntax]} {...props} ref={props.forwardedRef} />
   </>
 );
 
