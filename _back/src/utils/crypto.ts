216b3184a9b6f423879ad2de247520464fccb55f
chanyeong <cyjo9603@naver.com>
2021-01-16 20:38:12 +0900

    [delete] 기존 express server 제거
    - nestjs로 서버 변경 이후 기존 express server 제거

--- _back/src/utils/crypto.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto-js';
-
-const { LOGIN_SECRET_KEY } = process.env;
-
-export const encryptValue = (value: string) => crypto.AES.encrypt(value, LOGIN_SECRET_KEY!).toString();
-
-export const decryptValue = (value: string) => crypto.AES.decrypt(value, LOGIN_SECRET_KEY!).toString(crypto.enc.Utf8);
