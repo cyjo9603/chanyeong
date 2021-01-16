241e8b2a9416c92734799af6c2f492b656c11717
chanyeong <cyjo9603@naver.com>
2021-01-16 20:38:12 +0900

    [delete] 기존 express server 제거
    - nestjs로 서버 변경 이후 기존 express server 제거

--- _back/src/auth/passport/index.ts
+++
@@ -1,8 +0,0 @@
-import passport from 'passport';
-import local from '@auth/passport/local';
-import jwt from '@auth/passport/jwt';
-
-export default () => {
-  passport.use('local', local);
-  passport.use('jwt', jwt);
-};
