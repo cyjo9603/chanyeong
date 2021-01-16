803e6c9527f0ebd55a69d219df76fe6c5587aa2a
chanyeong <cyjo9603@naver.com>
2021-01-16 19:30:48 +0900

    [fix] 로그인 시 유저네임 반환하도록 설정
    - 로그인 시 유저네임 반환하도록 설정

--- back/src/auth/auth.resolver.ts
+++ back/src/auth/auth.resolver.ts
@@ -4,8 +4,9 @@ import { Response } from 'express';
 
 import { LocalAuthGuard } from '@auth/guards/local-auth.guard';
 import { User, TokenUser } from '@decorators/user.decorator';
-import { CoreResponse } from '@/common/dto/coreResponse.dto';
+import { CoreResponse } from '@common/dto/coreResponse.dto';
 import { UsersService } from '@users/users.service';
+import { User as UserModel } from '@users/users.model';
 import { AuthService } from './auth.service';
 import { SigninRequest, SigninResponse } from './dto/signin.dto';
 import { ExpriedJwtAuthGuard } from './guards/jwt-auth.guard';
@@ -25,12 +26,13 @@ export class AuthResolver {
     @Context() { res }: { res: Response },
   ): Promise<SigninResponse> {
     const accessToken = await this.authService.signin({ id: user.id });
+    const { familyName, givenName } = (await this.userService.getById(user.id)) as UserModel;
     res.cookie(jwtConstants.header, accessToken, {
       httpOnly: true,
       maxAge: EXPIRED,
     });
 
-    return { ok: true };
+    return { ok: true, userName: `${familyName}${givenName}` };
   }
 
   @UseGuards(ExpriedJwtAuthGuard)
