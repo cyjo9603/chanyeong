3f9d791932c30e69ae7034d3a89ba114f11ac577
chanyeong <cyjo9603@naver.com>
2021-01-24 19:34:57 +0900

    [feat] md-viewer에서 파싱 시 toc 반응 변수에 heading 추가
    - markdown 파싱 시 heading 태그들을 toc 반응 변수에 추가
    - markdown 컴포넌트 첫 렌더링시 toc 반응 변수 초기화

--- front/src/components/UI/organisms/MarkDownViewer/renderers/heading.tsx
+++ front/src/components/UI/organisms/MarkDownViewer/renderers/heading.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
+import { addToc } from '@store/postToc';
+
 interface HeadingProps {
   level: number;
   children: React.ReactChildren[];
 }
 
 const flatten = (text, child) => {
-  return typeof child === 'string' ? text + child : React.Children.toArray(child.props.children).reduce(flatten, text);
+  return typeof child === 'string'
+    ? text + child
+    : React.Children.toArray(child.props.children).reduce(flatten, text);
 };
 
 const heading = ({ level, children }: HeadingProps) => {
   const text = children.reduce(flatten, '');
   const slug = text.toLowerCase().replace(/\s/g, '-');
+  addToc({ level, content: text, slug });
   return React.createElement(`h${level}`, { id: slug }, children);
 };
 
