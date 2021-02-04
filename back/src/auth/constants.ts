af8448d4488657e031e14a52d537153589f15a01
chanyeong <cyjo9603@naver.com>
2021-02-04 21:41:11 +0900

    [refactor] 상수 constants 객체에 따로 저장
    - process.env에서 환경변수를 직접 참조하지 않고 상수 constants 객체에 따로 저장해 사용

--- back/src/auth/constants.ts
+++ back/src/auth/constants.ts
@@ -6,3 +6,7 @@ export const jwtConstants = {
   secret: process.env.JWT_SECRET_KEY!,
   header: process.env.JWT_HEADER!,
 };
+
+export const secretConstants = {
+  login: process.env.LOGIN_SECRET_KEY!,
+};
--- back/src/utils/crypto.ts
+++ back/src/utils/crypto.ts
@@ -1,7 +1,9 @@
 import crypto from 'crypto-js';
 
+import { secretConstants } from '@auth/constants';
+
 export const encryptValue = (value: string) =>
-  crypto.AES.encrypt(value, process.env.LOGIN_SECRET_KEY!).toString();
+  crypto.AES.encrypt(value, secretConstants.login).toString();
 
 export const decryptValue = (value: string) =>
-  crypto.AES.decrypt(value, process.env.LOGIN_SECRET_KEY!).toString(crypto.enc.Utf8);
+  crypto.AES.decrypt(value, secretConstants.login).toString(crypto.enc.Utf8);
