3a885fd97af159b03589a42e0a84e6f42e031c13
Chanyeong Cho <cyjo9603@naver.com>
2021-06-05 23:16:34 +0900

    배포 명령어 개선 (#53)
    * Chore: 스키마 생성 스크립트 제거
    
    * Chore: graphql schema gitignore 해제
    
    - 따로 스키마 생성 스크립트를 사용하지 않고 graphql schema 파일 사용
    
    * Chore: server pm2 ecosystem 적용
    
    * Deploy: server 배포 스크립트 작성
    
    * Chore: client pm2 ecosystem 적용
    
    * Deploy: server와 client 배포 스크립트 통합
    
    * Docs: .env example 추가
    
    * Chore: deploy script에 git pull 추가

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+# $1 back || front
+
+# project name
+PROJECT_NAME="chanyeong-$1"
+
+cd $1
+
+git pull
+
+# modules install
+yarn install
+echo 'yarn install'
+
+# front setting
+if [ $1 == 'front' ];
+then
+
+  if !(ls src | grep types)
+  then
+    mkdir src/types
+    echo 'mkdir types'
+  fi
+
+  yarn types
+  echo 'yarn types'
+fi
+
+# project build
+yarn build
+echo 'yarn build'
+
+# pm2 ecosystem deploy
+if pm2 list | grep -q ${PROJECT_NAME}
+then
+  pm2 reload pm2.yml --only ${PROJECT_NAME}
+  echo "pm2 reload"
+else
+  pm2 start pm2.yml --only ${PROJECT_NAME}
+  echo "pm2 start"
+fi
